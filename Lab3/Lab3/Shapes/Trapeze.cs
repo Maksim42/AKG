fffd323fa0b14093ccd3693e8a16e2a27cb55feb
Maksim42 <maksim.lomako@gmail.com>
2017-09-30 23:22:00 +0300

    L3: add Trapeze shape

---
+++ Lab3/Lab3/Shapes/Trapeze.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Shapes
+{
+    class Trapeze : Shape
+    {
+        /// <summary>
+        /// Trapeze constructor
+        /// </summary>
+        /// <param name="context">Shape context</param>
+        /// <param name="width">Shape width</param>
+        /// <param name="height">Shape height</param>
+        /// <param name="topProportion">Proportion top side to botom side</param>
+        /// <param name="x">Shape center X position</param>
+        /// <param name="y">Shape center Y position</param>
+        public Trapeze(WindowContext context, int width, int height, double topProportion, int x, int y)
+        {
+            this.context = context;
+            this.height = height;
+            this.width = width;
+            positionX = x;
+            positionY = y;
+
+            double halfWidth = width / 2.0;
+            double halfHeight = height / 2.0;
+            double topSide = width * topProportion;
+
+            points = new Point[] {
+                new Point(halfWidth, halfHeight),
+                new Point(halfWidth, -halfHeight),
+                new Point(-halfWidth, -halfHeight),
+                new Point(halfWidth - topSide, halfHeight)
+            };
+        }
+
+        public override bool AnimateMove(Shape linkShape)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw()
+        {
+            context.DrawLine(TransformPoint(points[0]),
+                             TransformPoint(points[1]));
+            context.DrawLine(TransformPoint(points[1]),
+                             TransformPoint(points[2]));
+            context.DrawLine(TransformPoint(points[2]),
+                             TransformPoint(points[3]));
+            context.DrawLine(TransformPoint(points[3]),
+                             TransformPoint(points[0]));
+        }
+
+        public override bool PointIn(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
