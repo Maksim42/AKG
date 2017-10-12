577fbe12acb3116f27c7c3c5ae690c85fc0f738c
Maksim42 <maksim.lomako@gmail.com>
2017-10-12 15:41:37 +0300

    L3: refactoring Point constructor

--- Lab3/Lab3/Point.cs
+++ Lab3/Lab3/Point.cs
@@ -6,15 +6,9 @@
         public int y;
 
         public Point(double x, double y) 
-            : this((int)x, (int)y)
         {
-
-        }
-
-        public Point(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
+            this.x = (int)x;
+            this.y = (int)y;
         }
     }
 }
