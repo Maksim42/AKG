b9bf663a8a6c7412f75be28252c1612c24e091ac
Maksim42 <maksim.lomako@gmail.com>
2017-11-23 20:02:57 +0300

    L4: mouse rotation on X and Y axis

--- Lab3/Lab4/MainWindow.cs
+++ Lab3/Lab4/MainWindow.cs
@@ -70,50 +70,60 @@ namespace SDLWindow
                                 quit = true;
                                 break;
                             }
-                        //case SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN:
-                        //    {
-                        //        MouseButtonDownHandler(sdlEvent);
-                        //        break;
-                        //    }
-                        //case SDL.SDL_EventType.SDL_MOUSEMOTION:
-                        //    {
-                        //        MouseMoveHandler(sdlEvent);
-                        //        break;
-                        //    }
-                        //case SDL.SDL_EventType.SDL_KEYDOWN:
-                        //    {
-                        //        var key = sdlEvent.key;
-                        //        switch (key.keysym.sym)
-                        //        {
-                        //            case SDL.SDL_Keycode.SDLK_a:
-                        //                {
-                        //                    A_KeyDownHandler();
-                        //                    break;
-                        //                }
-                        //            case SDL.SDL_Keycode.SDLK_UP:
-                        //                {
-                        //                    UP_KeyDownHandler();
-                        //                    break;
-                        //                }
-                        //            case SDL.SDL_Keycode.SDLK_DOWN:
-                        //                {
-                        //                    DOWN_KeyDownHandler();
-                        //                    break;
-                        //                }
-                        //            case SDL.SDL_Keycode.SDLK_RIGHT:
-                        //                {
-                        //                    RIGHT_KeyDownHandler();
-                        //                    break;
-                        //                }
-                        //            case SDL.SDL_Keycode.SDLK_LEFT:
-                        //                {
-                        //                    LEFT_KeyDownHandler();
-                        //                    break;
-                        //                }
+                        case SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN:
+                            {
+                                MouseButtonDownHandler(sdlEvent);
+                                break;
+                            }
+                        case SDL.SDL_EventType.SDL_MOUSEBUTTONUP:
+                            {
+                                MouseButtonUpHandler(sdlEvent);
+                                break;
+                            }
+                        case SDL.SDL_EventType.SDL_MOUSEMOTION:
+                            {
+                                MouseMoveHandler(sdlEvent);
+                                break;
+                            }
+                        case SDL.SDL_EventType.SDL_KEYDOWN:
+                            {
+                                var key = sdlEvent.key;
+                                switch (key.keysym.sym)
+                                {
+                                    case SDL.SDL_Keycode.SDLK_RETURN:
+                                        {
+                                            Enter_KeyDownHandler(sdlEvent);
+                                            break;
+                                        }
+                                        //case SDL.SDL_Keycode.SDLK_a:
+                                        //    {
+                                        //        A_KeyDownHandler();
+                                        //        break;
+                                        //    }
+                                        //case SDL.SDL_Keycode.SDLK_UP:
+                                        //    {
+                                        //        UP_KeyDownHandler();
+                                        //        break;
+                                        //    }
+                                        //case SDL.SDL_Keycode.SDLK_DOWN:
+                                        //    {
+                                        //        DOWN_KeyDownHandler();
+                                        //        break;
+                                        //    }
+                                        //case SDL.SDL_Keycode.SDLK_RIGHT:
+                                        //    {
+                                        //        RIGHT_KeyDownHandler();
+                                        //        break;
+                                        //    }
+                                        //case SDL.SDL_Keycode.SDLK_LEFT:
+                                        //    {
+                                        //        LEFT_KeyDownHandler();
+                                        //        break;
+                                        //    }
 
-                        //        }
-                        //        break;
-                        //    }
+                                }
+                                break;
+                            }
                     }
                 }
                 Draw();
@@ -129,7 +139,69 @@ namespace SDLWindow
         }
 
         #region Handlers
-        
+        int startX, startY;
+        bool mouseDrag = false;
+        bool animation = false;
+
+        private void MouseButtonDownHandler(SDL.SDL_Event e)
+        {
+            var mouseDownEvent = e.button;
+
+            // check left button click
+            if (mouseDownEvent.button != SDL.SDL_BUTTON_LEFT)
+            {
+                return;
+            }
+
+            startX = mouseDownEvent.x;
+            startY = mouseDownEvent.y;
+
+            mouseDrag = true;
+            animation = false;
+        }
+
+        private void MouseButtonUpHandler(SDL.SDL_Event e)
+        {
+            var mouseDownEvent = e.button;
+
+            // check left button click
+            if (mouseDownEvent.button != SDL.SDL_BUTTON_LEFT)
+            {
+                return;
+            }
+
+            mouseDrag = false;
+        }
+
+        private void MouseMoveHandler(SDL.SDL_Event e)
+        {
+            var moveEvent = e.motion;
+            int fullWidth, fullHeight;
+            SDL.SDL_GetWindowSize(window, out fullWidth, out fullHeight);
+
+            if (mouseDrag)
+            { 
+                shape.yAngle += rt(startX - moveEvent.x, fullWidth);
+                shape.xAngle += rt(startY - moveEvent.y, fullHeight);
+
+                startX = moveEvent.x;
+                startY = moveEvent.y;
+            }
+        }
+        private Func<double, double, double> rt = (l, f) => (l / f * 8);
+
+        private void Enter_KeyDownHandler(SDL.SDL_Event e)
+        {
+            var keyEvent = e.key;
+
+            if (keyEvent.repeat != 0)
+            {
+                return;
+            }
+
+            animation = !animation;
+            Console.WriteLine("E");
+        }
         #endregion Handlers
 
         private void Draw()
@@ -147,13 +219,16 @@ namespace SDLWindow
 
             shape.Draw();
 
-            shape.xAngle += 0.02;
-            shape.yAngle += 0.01;
-            shape.zAngle += 0.008;
-            shape.tAngle += 0.014;
-            //shape.Y += 0.1;
-            //shape.Z += 0.1;
-            //shape.Scale -= 0.01;
+            if (animation)
+            {
+                shape.xAngle += 0.02;
+                shape.yAngle += 0.01;
+                shape.zAngle += 0.007;
+                shape.tAngle += 0.013;
+                //shape.Y += 0.1;
+                //shape.Z += 0.1;
+                //shape.Scale -= 0.01;
+            }
 
             context.RefreshWindow();
         }
