25823cc9dab20480826049b0348bee4e31891143
Maksim42 <maksim.lomako@gmail.com>
2017-11-26 22:49:19 +0300

    L4: add Zbufer

---
+++ Lab3/Lab4/SDLGeometry/Zbufer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace SDLGeometry
+{
+    class Zbufer
+    {
+        private List<ZbuferItem> bufer;
+
+        public Zbufer()
+        {
+            bufer = new List<ZbuferItem>();
+        }
+
+        public void Add(Point position)
+        {
+            var exist = bufer.Find((i) => i.ComparePosition(position));
+
+            if (exist == null)
+            {
+                bufer.Add(new ZbuferItem(position));
+            }
+            else
+            {
+                exist.SetDepth(position.rX);
+            }
+        }
+
+        public bool Visible(Point position)
+        {
+            var exist = bufer.Find((i) => i.ComparePosition(position));
+
+            if (exist != null)
+            {
+                return exist.Position.rX <= position.rX;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void Unvalidating()
+        {
+            foreach(var item in bufer)
+            {
+                item.Unvalidate();
+            }
+        }
+    }
+}
---
+++ Lab3/Lab4/SDLGeometry/ZbuferItem.cs
@@ -0,0 +1,53 @@
+﻿namespace SDLGeometry
+{
+    class ZbuferItem
+    {
+        private bool valid;
+        private Point position;
+        private double depth;
+
+        public ZbuferItem(Point position)
+        {
+            this.position = new Point(position);
+            depth = position.rZ;
+            valid = true;
+        }
+
+        public Point Position => position;
+        public double Depth => depth;
+
+        /// <summary>
+        /// Change depth value if posible
+        /// </summary>
+        /// <param name="newDepth">New depth value</param>
+        public void SetDepth(double newDepth)
+        {
+            if (!valid)
+            {
+                depth = newDepth;
+                valid = true;
+                return;
+            }
+
+            if (newDepth > depth)
+            {
+                depth = newDepth;
+            }
+        }
+
+        /// <summary>
+        /// Check if position equivalent
+        /// </summary>
+        /// <param name="p">Another position</param>
+        /// <returns>True if position equivalent</returns>
+        public bool ComparePosition(Point p)
+        {
+            return (position.rX == p.rX && position.rY == p.rY);
+        }
+
+        public void Unvalidate()
+        {
+            valid = false;
+        }
+    }
+}
